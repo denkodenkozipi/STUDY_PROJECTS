aabfdbbfa2226d97ebc8dac86bb0096a134759da
amogus <144465902+denkodenkozipi@users.noreply.github.com>
2025-11-21 10:43:10 +0900

    Window 1.0
    stable ready-to-use version of Window

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import sys
 from PyQt6.QtWidgets import (
     QApplication,
     QWidget,
-    QLabel,       # Текстовая метка
+    QLabel,  # Текстовая метка
     QPushButton,  # Кнопка
-    QLineEdit,    # Поле для ввода текста
+    QLineEdit,  # Поле для ввода текста
     QVBoxLayout,  # Вертикальная компоновка
     QHBoxLayout,  # Горизонтальная компоновка
     QMessageBox,  # Уведомление
 )
 
 
+# noinspection PyUnresolvedReferences
 class Window(QWidget):
+
     def __init__(self):
         super().__init__()
 
@@ -19,94 +21,101 @@ class Window(QWidget):
         self.setWindowTitle("Авторизация")
         self.resize(400, 350)
 
+        # объявление атрибутов
+        self.line_user = None
+        self.line_psw = None
+        self.button_ok = None
+
         # присваивание
-        self.setLayout(self.UI_func())
+        self.setLayout(self.interface_manager())
 
-    def UI_func(self):
-        def Layout_hello_pls():
-            # метка приветствия
-            label_hello = QLabel("Добро пожаловать!")
-            label_pls = QLabel("Пожалуйста введите данные для авторизации")
+    @staticmethod
+    def layout_hello_manager():
+        # метка приветствия
+        label_hello = QLabel("Добро пожаловать!")
+        label_pls = QLabel("Пожалуйста введите данные для авторизации")
 
-            # компоновка преветствия по X
-            layout_hello = QHBoxLayout()
-            layout_hello.addStretch()
-            layout_hello.addWidget(label_hello)
-            layout_hello.addStretch()
+        # компоновка преветствия по X
+        layout_hello = QHBoxLayout()
+        layout_hello.addStretch()
+        layout_hello.addWidget(label_hello)
+        layout_hello.addStretch()
 
-            # компоновка просьбы по X
-            layout_pls = QHBoxLayout()
-            layout_pls.addStretch()
-            layout_pls.addWidget(label_pls)
-            layout_pls.addStretch()
+        # компоновка просьбы по X
+        layout_pls = QHBoxLayout()
+        layout_pls.addStretch()
+        layout_pls.addWidget(label_pls)
+        layout_pls.addStretch()
 
-            # компоновка приветствия и просьбы по Y
-            layout_hello_pls = QVBoxLayout()
-            layout_hello_pls.addStretch()
-            layout_hello_pls.addLayout(layout_hello)
-            layout_hello_pls.addLayout(layout_pls)
+        # компоновка приветствия и просьбы по Y
+        layout_hello_pls = QVBoxLayout()
+        layout_hello_pls.addStretch()
+        layout_hello_pls.addLayout(layout_hello)
+        layout_hello_pls.addLayout(layout_pls)
 
-            return layout_hello_pls
+        return layout_hello_pls
 
-        def Layout_user():
-            # метки связанные с user
-            label_user = QLabel("Введите имя")
-            line_user = QLineEdit("Иван")
+    def layout_user_manager(self):
+        # метки связанные с user
+        label_user = QLabel("Введите имя")
+        self.line_user = QLineEdit("Иван")
 
-            # компоновка по таблички "введите имя" по X
-            layout_user_h = QHBoxLayout()
-            layout_user_h.addStretch()
-            layout_user_h.addWidget(label_user)
-            layout_user_h.addStretch()
+        # компоновка по таблички "введите имя" по X
+        layout_user_h = QHBoxLayout()
+        layout_user_h.addStretch()
+        layout_user_h.addWidget(label_user)
+        layout_user_h.addStretch()
 
-            # компоновка имени и вводного поля по Y
-            layout_user_v = QVBoxLayout()
-            layout_user_v.addStretch()
-            layout_user_v.addLayout(layout_user_h)
-            layout_user_v.addWidget(line_user)
-            layout_user_v.addStretch()
+        # компоновка имени и вводного поля по Y
+        layout_user_v = QVBoxLayout()
+        layout_user_v.addStretch()
+        layout_user_v.addLayout(layout_user_h)
+        layout_user_v.addWidget(self.line_user)
+        layout_user_v.addStretch()
 
-            return layout_user_v
+        return layout_user_v
 
-        def Layout_psw():
-            # метки связанные с Password
-            label_psw = QLabel("Введите пароль")
-            line_psw = QLineEdit("12345")
+    def layout_psw_manager(self):
+        # метки связанные с Password
+        label_psw = QLabel("Введите пароль")
+        self.line_psw = QLineEdit("12345")
 
-            # компоновка таблички "введите пароль" по X
-            layout_psw_h = QHBoxLayout()
-            layout_psw_h.addStretch()
-            layout_psw_h.addWidget(label_psw)
-            layout_psw_h.addStretch()
+        # компоновка таблички "введите пароль" по X
+        layout_psw_h = QHBoxLayout()
+        layout_psw_h.addStretch()
+        layout_psw_h.addWidget(label_psw)
+        layout_psw_h.addStretch()
 
-            # компоновка таблички и вводного поля по Y
-            layout_psw_v = QVBoxLayout()
-            layout_psw_v.addStretch()
-            layout_psw_v.addLayout(layout_psw_h)
-            layout_psw_v.addWidget(line_psw)
-            layout_psw_v.addStretch()
+        # компоновка таблички и вводного поля по Y
+        layout_psw_v = QVBoxLayout()
+        layout_psw_v.addStretch()
+        layout_psw_v.addLayout(layout_psw_h)
+        layout_psw_v.addWidget(self.line_psw)
+        layout_psw_v.addStretch()
 
-            return layout_psw_v
+        return layout_psw_v
 
-        def Layout_button():
-            # метка кнопки
-            button_ok = QPushButton("Подтвердить")
+    def layout_button_manager(self):
+        # метка кнопки
+        self.button_ok = QPushButton("Подтвердить")
 
-            # компоновка кнопки по X
-            layout_button = QHBoxLayout()
-            layout_button.addStretch()
-            layout_button.addWidget(button_ok)
-            layout_button.addStretch()
+        # компоновка кнопки по X
+        layout_button = QHBoxLayout()
+        layout_button.addStretch()
+        layout_button.addWidget(self.button_ok)
+        layout_button.addStretch()
 
-            self.button_ok.clicked.connect(self.check_data)
+        self.button_ok.clicked.connect(self.check_data)
 
-            return layout_button
+        return layout_button
+
+    def interface_manager(self):
 
         # присваивание функций
-        layout_hello_pls = Layout_hello_pls()
-        layout_user = Layout_user()
-        layout_psw = Layout_psw()
-        layout_button = Layout_button()
+        layout_hello_pls = self.layout_hello_manager()
+        layout_user = self.layout_user_manager()
+        layout_psw = self.layout_psw_manager()
+        layout_button = self.layout_button_manager()
 
         # компоновка всех layout по горизонтали
         layout_h = QHBoxLayout()
@@ -125,8 +134,23 @@ class Window(QWidget):
         return layout_main
 
     def check_data(self):
-        def call_not():
+        # сохранение данные из поля
+        data_user = self.line_user.text()
+        data_psw = self.line_psw.text()
 
+        # ошибка пустого ввода
+        if not data_user or not data_psw:
+            QMessageBox.warning(self, "Ошибка!", "Введите данные")
+            return
+
+        # если логин и пароль верны, то успешно
+        if data_user == "Иван" and data_psw == '12345':
+            QMessageBox.information(self, "Успех", "Авторизация прошла успешно!")
+            return
+        # если логин или пароль не верны ошибка
+        else:
+            QMessageBox.critical(self, "Ошибка", "Не верный логин или пароль!")
+            return
 
 
 if __name__ == "__main__":
